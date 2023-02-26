069d0c4cb3f61bc4820990c9002ad9f291ae0ce0
IggyWright <iggy.wright81@gmail.com>
2023-02-25 23:46:05 -0500

    Update styling and add google analytics

--- src/frontend/code_accuracy/pages/_app.js
+++ src/frontend/code_accuracy/pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/reset.css'
-// import '../styles/globals.css'
 import '@/styles/globals.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
+
+
+
 export default function App({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+        ga.pageview(url)
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
+
   return <Component {...pageProps} />
 }
